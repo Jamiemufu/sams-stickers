2fd42dc50175dc7eb999899ccdbfa674ccc3369e
Jamie Evans <taichi.chikung@gmail.com>
2022-01-11 10:49:21 +0000

    Add ALL category

--- client/components/atoms/TheNav.vue
+++ client/components/atoms/TheNav.vue
@@ -1,6 +1,15 @@
 <template>
   <nav>
     <ul class="[] [ flex flex-row flex-wrap py-7 ]">
+      <!-- add manual 'all' link to navigation -->
+      <li
+        class="[] [ bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded shadow ]"
+      >
+        <nuxt-link :to="`/categories/all`">
+         All
+        </nuxt-link>
+      </li>
+      <!-- all other categories in CMS -->
       <li
         class="[] [ bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded shadow ]"
         v-for="category in categories.data"
--- client/pages/categories/_id.vue
+++ client/pages/categories/_id.vue
@@ -11,13 +11,19 @@
 
 <script>
 import { useFetch, useRoute, ref } from "@nuxtjs/composition-api";
-import { flattenStickers }  from "~/utils/flatten";
-import { queryNestedAndFilterByCategory } from "~/utils/queries";
+import { flattenStickers } from "~/utils/flatten";
+import {
+  queryNestedAndFilterByCategory,
+  queryNestedStickerEntry,
+} from "~/utils/queries";
 export default {
   setup() {
     const route = useRoute();
     const stickers = ref();
-    const params = queryNestedAndFilterByCategory('sticker',route.value.params.id);
+    const params =
+      route.value.params.id === "all"
+        ? queryNestedStickerEntry()
+        : queryNestedAndFilterByCategory("sticker", route.value.params.id);
     /**
      * Query for stickers that belongs to category from slug
      * populate stickers - add media relationship to response
